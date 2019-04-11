0e3c2e1d64a94f786b0102b9ec1df361a94839a8
Michael Kapelko <kornerr@gmail.com>
2019-04-11 19:01:12 +0300

    Start HelloWorld.java class impl

---
+++ bundles/commons/src/main/java/com/adobe/cq/sample/spa/commons/impl/models/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.adobe.cq.sample.spa.commons.impl.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+
+public class HelloWorld
+    implements ComponentExporter
+{
+    static final String RESOURCE_TYPE = "we-retail-journal/components/helloworld";
+}
